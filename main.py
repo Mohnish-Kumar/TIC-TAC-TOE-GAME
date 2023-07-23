d81e3979e89c500bff8a2f2cd227d33900bffb94
Mohnish Kumar <mohnishkumar047@gmail.com>
2023-07-23 15:51:13 +0530

    Added the condition to check the draw.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print()
-def sum(a,b,c):
-    return a+b+c
+def sum(a, b, c):
+    return a + b + c
 
-def printBoard(xState,zState):
+def printBoard(xState, zState):
     one='X' if xState[6] else ('O' if zState[6] else 1)
     two='X' if xState[7] else ('O' if zState[7] else 2)
     three='X' if xState[8] else ('O' if zState[8] else 3)
@@ -19,42 +19,50 @@ def printBoard(xState,zState):
     print(f"---|---|---")
     print(f" {one} | {two} | {three} ")
 
-def checkWin(xState,zState,player1,player2):
-    wins=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[6,7,8],[0,4,8],[2,4,6]]
+def checkWin(xState, zState, player1, player2):
+    wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
+    
+    # Check for draw scenario
+    if 0 not in [xState[i] + zState[i] for i in range(len(xState))]:
+        print("It's a draw!")
+        return 10
+    
     for win in wins:
-        if sum(xState[win[0]],xState[win[1]],xState[win[2]])==3:
+        if sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3:
             print(f"{player1} won the match!")
             return 1
-        if sum(zState[win[0]],zState[win[1]],zState[win[2]])==3:
+        if sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3:
             print(f"{player2} won the match!")
             return 0
+
     return -1
 
-numberChange={7:0,8:1,9:2,4:3,5:4,6:5,1:6,2:7,3:8}
+numberChange = {7: 0, 8: 1, 9: 2, 4: 3, 5: 4, 6: 5, 1: 6, 2: 7, 3: 8}
 
-if __name__=="__main__":
-    player1=input("Enter Player 1's name : ")
-    player2=input("Enter Player 2's name : ")
+if __name__ == "__main__":
+    player1 = input("Enter Player 1's name : ")
+    player2 = input("Enter Player 2's name : ")
     print()
-    xState=[0,0,0,0,0,0,0,0,0]
-    zState=[0,0,0,0,0,0,0,0,0]
-    turn=1 # 1 for X and 0 for O
-    print("Welcome to Tic Tac Toe")  
+    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1  # 1 for X and 0 for O
+    print("Welcome to Tic Tac Toe")
     print()
-    printBoard(xState,zState)  
+    printBoard(xState, zState)
     while True:
         if turn == 1:
             print(f"{player1}'s Chance, put X")
-            value=int(input("Please enter a value : "))
+            value = int(input("Please enter a value : "))
             print()
-            xState[numberChange[value]]=1
+            xState[numberChange[value]] = 1
         else:
             print(f"{player2}'s Chance, put O")
-            value=int(input("Please enter a value : "))
+            value = int(input("Please enter a value : "))
             print()
-            zState[numberChange[value]]=1
-        printBoard(xState,zState)
-        if checkWin(xState,zState,player1,player2)!=-1:
+            zState[numberChange[value]] = 1
+        printBoard(xState, zState)
+        result = checkWin(xState, zState, player1, player2)
+        if result != -1:
             break
-        turn=1-turn
-print("Match Over!")
+        turn = 1 - turn
+    print("Match Over!")
